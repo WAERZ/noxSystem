df14a05323aa705d5d8b795972756ddf7efdb7bc
WAERZ <walterz@inbox.ru>
2016-04-15 02:29:43 +0300

    push

---
+++ main.php
@@ -0,0 +1,7 @@
+<?php
+define('ROOT',dirname(__FILE__).'/');
+define('SYSTEM',dirname(__FILE__).'/system/');
+define('APP',dirname(__FILE__).'/application/');
+define('CLASSES',dirname(__FILE__).'/system/classes/');
+
+?>
